46da8304605bee8890d3fc967a3993227a8c2b90
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-11-16 23:29:55 +0300

    🐛 fix: forgot to remove hrefs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,16 +10,13 @@ const Header = () => {
         <>
             <Navbar className="header" expand="lg">
                 <Container>
-                    <Navbar.Brand href="/" className="text-white fs-2">
+                    <Navbar.Brand className="text-white fs-2">
                         <Link to={ROUTES.HOME}>KiloGram</Link>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto text-white fs-5">
-                            <Nav.Link
-                                href="/recipients/"
-                                className="text-white"
-                            >
+                            <Nav.Link className="text-white">
                                 <Link to={ROUTES.RECIPIENTS}>Получатели</Link>
                             </Nav.Link>
                         </Nav>
