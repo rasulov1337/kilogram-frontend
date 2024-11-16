34349b64e3663b56377304f13acd53bb54eb50cf
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-11-16 23:39:03 +0300

    🚀 build: add conf to build the project on gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import RecipientPage from "./components/ProfilePage/RecipientPage";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RecipientsPage from "./components/RecipientsPage/RecipientsPage";
 import StartPage from "./components/StartPage/StartPage";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/kilogram-frontend">
             <Header></Header>
 
             <Routes>
