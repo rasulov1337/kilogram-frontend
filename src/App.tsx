ffacb351cc887402db48b44f783cef99266cf987
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-04 14:06:56 +0300

    feat: add tauri dev support

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ import RecipientPage from "./pages/ProfilePage/RecipientPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RecipientsPage from "./pages/RecipientsPage/RecipientsPage";
 import StartPage from "./pages/StartPage/StartPage";
+import { useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
+
 
 function App() {
+    useEffect(()=>{
+        invoke('tauri', {cmd:'create'})
+          .then(() =>{console.log("Tauri launched")})
+          .catch(() =>{console.log("Tauri not launched")})
+        return () =>{
+          invoke('tauri', {cmd:'close'})
+            .then(() =>{console.log("Tauri launched")})
+            .catch(() =>{console.log("Tauri not launched")})
+        }
+      }, [])
+      
     return (
         <BrowserRouter basename="/kilogram-frontend/">
             <Header></Header>
