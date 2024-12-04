1e8d280de64699647e81cfc8771691f3813ebc37
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-04 13:18:17 +0300

    fix: append / to the base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/kilogram-frontend",
+    base: "/kilogram-frontend/",
     server: {
         host: "0.0.0.0",
         proxy: {
