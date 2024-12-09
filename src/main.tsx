06ab3cd51a0c6be09f5f8f612e29e7ad6831dc9c
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-10 00:02:25 +0300

    fix: prevent duplicate GET requests

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,9 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App></App>
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <App></App>
+    </Provider>
 );
 
 if ("serviceWorker" in navigator) {
