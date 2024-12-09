850d455fc1cea0f55ade1708d84ad5f19190fa46
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-10 01:11:32 +0300

    fix: correct DEST_ROOT

--- src/target_config.ts
+++ src/target_config.ts
@@ -1,6 +1,6 @@
 const target_tauri = true;
 
-export const DEST_ROOT = target_tauri ? "/" : "/kilogram-frontend";
+export const DEST_ROOT = target_tauri ? "/" : "/kilogram-frontend/";
 export const API_PROXY_ADDR = "http://192.168.119.37:8000/";
 export const IMG_PROXY_ADDR = "http://192.168.119.37:9000";
 export const DEST_API = target_tauri ? API_PROXY_ADDR : "/api/";
