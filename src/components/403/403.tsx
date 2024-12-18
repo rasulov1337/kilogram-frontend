e76cce26d93c04f3fa10b4741c88e45ede896610
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-18 16:17:43 +0300

    feat: add button for returning to start page

--- src/components/403/403.tsx
+++ src/components/403/403.tsx
@@ -1,9 +1,16 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../modules/Routes";
+
 export default function Page403() {
     return (
         <>
             <h2 style={{ textAlign: "center", marginTop: "30vh" }}>
                 403: Доступ запрещен
             </h2>
+            <Button style={{ display: "block", margin: "0 auto" }}>
+                <Link to={ROUTES.HOME}>Вернуться на главную</Link>
+            </Button>
         </>
     );
 }
--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -1,9 +1,17 @@
+import { Button } from "react-bootstrap";
+import { ROUTES } from "../../modules/Routes";
+import { Link } from "react-router-dom";
+
 export default function Page404() {
     return (
         <>
             <h2 style={{ textAlign: "center", marginTop: "30vh" }}>
                 404: Страница не найдена
             </h2>
+
+            <Button style={{ display: "block", margin: "0 auto" }}>
+                <Link to={ROUTES.HOME}>Вернуться на главную</Link>
+            </Button>
         </>
     );
 }
