0ad26f50add54bb4ecd28d291b07dee11c5b4229
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-18 16:01:57 +0300

    fix: update address

--- src/target_config.ts
+++ src/target_config.ts
@@ -1,7 +1,7 @@
 const target_tauri = true;
 
 export const DEST_ROOT = target_tauri ? "/" : "/kilogram-frontend/";
-export const API_PROXY_ADDR = "http://192.168.119.37:8000/";
-export const IMG_PROXY_ADDR = "http://192.168.119.37:9000";
+export const API_PROXY_ADDR = "http://192.168.119.141:8000/";
+export const IMG_PROXY_ADDR = "http://192.168.119.141:9000";
 export const DEST_API = target_tauri ? API_PROXY_ADDR : "/api/";
 export const DEST_IMG = target_tauri ? IMG_PROXY_ADDR : "/images/";
